cdbbe2a70b47e44dac84cae2cfd7a68f3cd13359
AnnaStogniy <97921315+AnnaStogniy@users.noreply.github.com>
2022-08-02 23:44:44 +0300

    Ended changing the celsium to the fahreinheit

--- src/app.js
+++ src/app.js
@@ -33,68 +33,69 @@ function currentTime() {
     event.preventDefault();
     currentCity.innerHTML = `${inputNewCity.value}`;
   
-    //Change all temperature additions
-    function getNewCity(response) {
-      let mathRoundedTemp = Math.round(response.data.main.temp);
-      let wind = document.querySelector("#wind");
-      let humidity = document.querySelector("#humidity");
-      let description = document.querySelector("#description");
-
-      currentTemperature.innerHTML = `${mathRoundedTemp}`; 
-     description.innerHTML = `${response.data.weather[0].description}`;
-     humidity.innerHTML = `Humidity: ${response.data.main.humidity} %`
-     wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`
-
-     //change image and colour
-     let descriptionToImage = response.data.weather[0].main;
-     if (descriptionToImage === "Clear"){
-      document.getElementById("image").src = "img/sun.png";
-      document.getElementById("fullPage").style.background = "linear-gradient(110.3deg, rgb(238, 179, 123) 8.7%, rgb(216, 103, 77) 47.5%, rgb(114, 43, 54) 89.1%)";
-     }
-
-     if (descriptionToImage === "Clouds"){
-      document.getElementById("image").src = "img/sunwithclouds.png";
-      document.getElementById("fullPage").style.background = "linear-gradient(109.6deg, rgb(0, 37, 84) 11.2%, rgba(0, 37, 84, 0.32) 100.2%)";
-     }
-
-     if (descriptionToImage === "Rain"){
-      document.getElementById("image").src = "img/rainy-day.png";
-      document.getElementById("fullPage").style.background = "linear-gradient(111.4deg, rgb(55, 168, 192) -0.2%, rgb(103, 187, 125) 100.2%)";
-     }
-
-     if (descriptionToImage === "Snow"){
-      document.getElementById("image").src = "img/snowflake.png";
-      document.getElementById("fullPage").style.background = "linear-gradient(111.5deg, rgb(187, 232, 255) 11.1%, rgb(4, 78, 116) 107.5%)";
-     }
-
-     if (descriptionToImage === "Thunderstorm"){
-      document.getElementById("image").src = "img/thunderstorm.png";
-      document.getElementById("fullPage").style.background = "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
-     }
-    }
-  
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputNewCity.value}&appid=648a7061e4cc76852d26d41d4a66634b&units=metric`;
     axios.get(apiUrl).then(getNewCity);
   }
 
+  //Change all temperature additions
+  function getNewCity(response) {
+     celsiusTemp = Math.round(response.data.main.temp);
+    let wind = document.querySelector("#wind");
+    let humidity = document.querySelector("#humidity");
+    let description = document.querySelector("#description");
+
+    currentTemperature.innerHTML = `${celsiusTemp}`; 
+   description.innerHTML = `${response.data.weather[0].description}`;
+   humidity.innerHTML = `Humidity: ${response.data.main.humidity} %`
+   wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`
+
+   //change image and colour
+   let descriptionToImage = response.data.weather[0].main;
+   if (descriptionToImage === "Clear"){
+    document.getElementById("image").src = "img/sun.png";
+    document.getElementById("fullPage").style.background = "linear-gradient(110.3deg, rgb(238, 179, 123) 8.7%, rgb(216, 103, 77) 47.5%, rgb(114, 43, 54) 89.1%)";
+   }
+
+   if (descriptionToImage === "Clouds"){
+    document.getElementById("image").src = "img/sunwithclouds.png";
+    document.getElementById("fullPage").style.background = "linear-gradient(109.6deg, rgb(0, 37, 84) 11.2%, rgba(0, 37, 84, 0.32) 100.2%)";
+   }
+
+   if (descriptionToImage === "Rain"){
+    document.getElementById("image").src = "img/rainy-day.png";
+    document.getElementById("fullPage").style.background = "linear-gradient(111.4deg, rgb(55, 168, 192) -0.2%, rgb(103, 187, 125) 100.2%)";
+   }
+
+   if (descriptionToImage === "Snow"){
+    document.getElementById("image").src = "img/snowflake.png";
+    document.getElementById("fullPage").style.background = "linear-gradient(111.5deg, rgb(187, 232, 255) 11.1%, rgb(4, 78, 116) 107.5%)";
+   }
+
+   if (descriptionToImage === "Thunderstorm"){
+    document.getElementById("image").src = "img/thunderstorm.png";
+    document.getElementById("fullPage").style.background = "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
+   }
+  }
   let form = document.querySelector("#change-city");
-  form.addEventListener("submit", changeCity, changeCelsius);
+  form.addEventListener("submit", changeCity);
 
   let currentTemperature = document.querySelector("#current-temperature");
 
   function changeCelsius(event) {
     event.preventDefault();
-    currentTemperature.innerHTML = `${response.data.main.temp}`;
+    currentTemperature.innerHTML = celsiusTemp;
   }
   let newCelsius = document.querySelector("#current-celcius");
   newCelsius.addEventListener("click", changeCelsius);
   
-  function changeFahrenheit(event, fa) {
+  function changeFahrenheit(event) {
     event.preventDefault();
-    currentTemperature.innerHTML = `${fa}`;
+
+    let fahrenheit = Math.round(celsiusTemp* 9 / 5 + 32);
+
+    currentTemperature.innerHTML = `${fahrenheit}`;
   }
-  let fa = `${mathRoundedTemp*9/5+32}`
+  let celsiusTemp = null;
 
-  let fahrenheit = document.querySelector("#current-fahrenheit");
-  fahrenheit.addEventListener("click", changeFahrenheit);
-
+  let fahrenheitElement = document.querySelector("#current-fahrenheit");
+  fahrenheitElement.addEventListener("click", changeFahrenheit);
